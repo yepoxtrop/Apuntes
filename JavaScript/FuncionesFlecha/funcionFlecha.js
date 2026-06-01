a69f4017990b6732c4ac8b920afcff849690f18e
yepoxtrop <sarmientodiazluisangel@gmail.com>
2026-06-01 16:06:06 -0500

    Se agregan las secion de Funciones flecha

---
+++ JavaScript/FuncionesFlecha/funcionFlecha.js
@@ -0,0 +1,17 @@
+//Funcion normal
+function mensaje() {
+    console.log("Hola mundo");
+}
+mensaje();
+
+//Funcion anónima
+const mensajeAnonimo = function() {
+    console.log("Hola mundo desde la función anónima");
+}
+mensajeAnonimo();
+
+//Funcion flecha
+const mensajeFlecha = () => {
+    console.log("Hola mundo desde la función flecha");
+}
+mensajeFlecha();
